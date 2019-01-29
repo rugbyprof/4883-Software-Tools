ec5911c90b6e440141bd32e254d60959e3d50df5
Terry Griffin <griffin@Terrys-MacBook-Pro.local>
2019-01-29 16:24:21 -0600

    Pushed from Terrys-MacBook-Pro

--- Assignments/A03/scrape_nfl.py
+++ Assignments/A03/scrape_nfl.py
@@ -1,15 +1,82 @@
 from beautifulscraper import BeautifulScraper
 from pprint import pprint
-scraper = BeautifulScraper()
+from time import sleep
+import json
+import urllib
 
-url = "http://www.nfl.com/schedules/2009/REG1"
-page = scraper.go(url)
+# scraper = BeautifulScraper()
 
-divs = page.find_all('div',{"class":"schedules-list-content"})
+# years  = [x for x in range(2009,2019)]
+# years  = [x for x in range(2009,2012)]
 
-print(divs)
+# weeks = [x for x in range(1,18)]
+# weeks = [x for x in range(1,5)]
 
-for div in divs:
-    if "type-reg" in div:
-        print(div)
+# stype = "REG"
 
+# gameids = {
+#     "REG":{},
+#     "POST":{}
+#     }
+
+# for year in years:
+#     gameids["REG"][year] = {}
+#     for week in weeks:
+#         gameids["REG"][year][week] = []
+#         url = "http://www.nfl.com/schedules/%d/%s%s" % (year,stype,str(week))
+
+#         print(url)
+#         page = scraper.go(url)
+
+#         divs = page.find_all('div',{"class":"schedules-list-content"})
+
+#         for div in divs:
+#             gameids["REG"][year][week].append(div['data-gameid'])
+#         sleep(.02)
+
+#     gameids["POST"][year] = []
+#     url = "http://www.nfl.com/schedules/%d/%s" % (year,"POST")
+#     print(url)
+#     page = scraper.go(url)
+
+#     divs = page.find_all('div',{"class":"schedules-list-content"})
+
+#     for div in divs:
+#         gameids["POST"][year].append(div['data-gameid'])
+#     sleep(.02)
+
+
+# pprint(gameids)
+# pprint(gameids.keys())
+
+# for year in gameids:
+#     print(gameids[year].keys())
+
+# f = open("nfldata.json","w")
+
+# f.write(json.dumps(gameids))
+# f.close()
+
+if __name__=='__main__':
+
+    with open('nfldata.json') as f:
+        data = json.load(f)
+
+    for season_type,subdictionary in data.items():
+        print(season_type)
+        if season_type == "REG":
+            for year,week_dictionary in subdictionary.items():
+                print(year)
+                for week,gameids in week_dictionary.items():
+                    print(week)
+                    for game in gameids:
+                        print(game)
+                
+        else:
+            for year,gameids in subdictionary.items():
+                print(year)
+                for game in gameids:
+                    print(game)
+                    print("http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (game,game))
+                    url = "http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (game,game)
+                    urllib.request.urlretrieve(url,'nfl_data/'+game+'.json')
