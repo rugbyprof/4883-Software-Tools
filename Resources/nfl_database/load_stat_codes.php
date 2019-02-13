62050208d3e998eddfaa30f64b83dd674b46ce8d
rugbyprof <terry.griffin@mwsu.edu>
2019-02-13 12:42:11 -0600

    Pushed from BO128B-49502

---
+++ Resources/nfl_database/load_stat_codes.php
@@ -0,0 +1,26 @@
+<?php
+$config = [
+    'servername' => "localhost",
+    'username' => "nfl_data",
+    'password' => "xDYfaR0rPomiQY6M",
+    'dbname' => "nfl_data"
+];
+
+// Create connection
+$conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+$stat_codes = json_decode(file_get_contents("stat_codes.json"),true);
+
+foreach($stat_codes as $id => $code){
+    $n = addslashes ($code['Name']);
+    $c = addslashes ($code['Comment']);
+    $sql = "INSERT INTO `stat_codes` VALUES ('$id','{$n}','{$c}')";
+    $result = $conn->query($sql);
+    echo"$sql\n";
+}
